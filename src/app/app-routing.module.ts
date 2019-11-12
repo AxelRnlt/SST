559e47db3f6f4dc700bef4099d7fa46755bfcaa5
Brugie <aaa@bbb.fr>
2019-11-12 16:55:05 +0100

    msg

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ const routes: Routes = [
     path: '',
     loadChildren: () => import('./SST/SST.module').then(m => m.SSTPageModule)
   },
-   { path: 'card1', loadChildren: './Accueil/card1/card1.module#Card1PageModule' } 
+  /* { path: 'card1', loadChildren: './Accueil/card1/card1.module#Card1PageModule' } */
 ]; 
 @NgModule({
   imports: [
